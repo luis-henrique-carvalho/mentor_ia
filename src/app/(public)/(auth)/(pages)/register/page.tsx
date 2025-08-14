dcf99c2b14349de2f398becd5848eada1e5e3395
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-08-13 22:48:06 -0300

    Enhance login and registration page layouts
    - Increased the maximum width of the container on both Login and Register pages for improved layout and user experience.
    - Maintained existing styles and structure while optimizing for larger screens.

--- src/app/(public)/(auth)/(pages)/register/page.tsx
+++ src/app/(public)/(auth)/(pages)/register/page.tsx
@@ -3,7 +3,7 @@ import { RegisterForm } from "@/app/(public)/(auth)/components/register-form";
 export default function RegisterPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+      <div className="w-full max-w-sm md:max-w-4xl">
         <RegisterForm />
       </div>
     </div>
